cac54b94ba51ad764dad1d8578857fabe5aaf059
Crequer, Jimmy | Jim | ECFD <jimmy.crequer@rakuten.com>
2020-02-03 11:57:54 +0900

    Add recipes scraper

--- scripts/scrape-escha-and-logy.js
+++ scripts/scrape-escha-and-logy.js
@@ -4,60 +4,132 @@
     const cheerio = require('cheerio')
     const neo4j = require('neo4j-driver')
 
+
     require('dotenv').config()
+
 
     const driver = neo4j.driver(`bolt://${process.env.NEO4J_HOST}`, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PWD))
 
     const BASE_URL = 'https://barrelwisdom.com'
 
-    const res = await axios(BASE_URL + '/escha/Materials')
-    const $ = cheerio.load(res.data)
 
-    const rows = $('#mw-content-text > table > tbody > tr')
-    const materials = []
-    rows.each((i, row) => {
-        if (i == 0)
-            return
+    await importMaterials()
+    await importRecipes()
 
-        const uri = $(':nth-child(1) a', row).attr('href')
-        materials.push({ uri })
-    })
 
-    for await (const m of materials) {
-        const ress = await axios(BASE_URL + m.uri)
-        const $$ = cheerio.load(ress.data)
-
-        m.name = $$('#firstHeading').text()
-
-        const categoriesCell = $$('#mw-content-text > table:nth-child(2) > tbody > tr > td:contains("Categories")').next()
-        m.categories = $$('a', categoriesCell).contents()
-                .filter(function() { return this.nodeType === 3 })
-                .map((j, el) => el.data)
-                .toArray()
-
-        console.log(`Finished: ${m.name}`)
-        await new Promise(r => setTimeout(r, 50));
-    }
-
-    console.log(materials)
-
-    var session = driver.session()
-
-    result = await session.run(`
-        UNWIND $materials AS material 
-        MERGE (m:Material {name: material.name })
-        FOREACH (category IN material.categories |
-            MERGE (c:Category {name: category})
-            MERGE (m)-[:IS]->(c)
-        )
-        `, { materials }
-    )
-
-    await session.close()
     await driver.close()
 
 
+    async function importMaterials() {
+        const materials = []
 
+        let res = await axios(BASE_URL + '/escha/Materials')
+        let $ = cheerio.load(res.data)
+    
+        const rows = $('#mw-content-text > table > tbody > tr')
+        rows.each((i, row) => {
+            if (i == 0)
+                return
+    
+            const uri = $(':nth-child(1) a', row).attr('href')
+            materials.push({ uri })
+        })
+    
+        for (let i = 0; i < materials.length; i++) {
+            const m = materials[i]
+    
+            res = await axios(BASE_URL + m.uri)
+            $ = cheerio.load(res.data)
+    
+            m.name = $('#firstHeading').text()
+    
+            const categoriesCell = $('#mw-content-text > table:nth-child(2) > tbody > tr > td:contains("Categories")').next()
+            m.categories = $('a', categoriesCell).contents()
+                    .filter(function() { return this.nodeType === 3 })
+                    .map((j, el) => el.data)
+                    .toArray()
+    
+            console.log(`Finished: ${m.name} (${i+1}/${materials.length})`)
+            await new Promise(r => setTimeout(r, 10));
+        }
+    
+        var session = driver.session()
+    
+        result = await session.run(`
+            UNWIND $materials AS material 
+            MERGE (m:Material {name: material.name })
+            FOREACH (category IN material.categories |
+                MERGE (c:Category {name: category})
+                MERGE (m)-[:IS]->(c)
+            )
+            `, { materials }
+        )
+    
+        await session.close()
+    }
+
+    async function importRecipes() {
+        const recipes = []
+
+        let res = await axios(BASE_URL + '/escha/Recipe_Books')
+        let $ = cheerio.load(res.data)
+    
+        const rows = $('#mw-content-text > table > tbody > tr')
+        rows.each((i, row) => {
+            if (i == 0)
+                return
+    
+            $('td.Recipes.smwtype_wpg a', row).each((j, a) => {
+                const uri = $(a).attr('href')
+                recipes.push({ uri })
+            })  
+        })
+    
+        for (let i = 0; i < recipes.length; i++) {
+            const r = recipes[i]
+    
+            res = await axios(BASE_URL + r.uri)
+            $ = cheerio.load(res.data)
+    
+            r.name = $('#firstHeading').text()
+
+            const ingredientsCell = $('#mw-content-text > table:nth-child(2) > tbody > tr:nth-child(5) > td:nth-child(1)').next()
+            r.ingredients = $('a', ingredientsCell).contents()
+                    .filter(function() { return this.nodeType === 3 })
+                    .map((j, el) => el.data)
+                    .toArray()
+    
+            const categoriesCell = $('#mw-content-text > table:nth-child(2) > tbody > tr > td:contains("Categories")').next()
+            r.categories = $('a', categoriesCell).contents()
+                    .filter(function() { return this.nodeType === 3 })
+                    .map((j, el) => el.data)
+                    .toArray()
+    
+            console.log(`Finished: ${r.name} (${i+1}/${recipes.length})`)
+            await new Promise(r => setTimeout(r, 10));
+        }
+
+        var session = driver.session()
+
+        result = await session.run(`
+            UNWIND $recipes AS recipe 
+            MERGE (r:Recipe {name: recipe.name })
+            
+            WITH r, recipe
+            UNWIND recipe.ingredients AS ingredient
+            MATCH (i {name: ingredient})
+            MERGE (r)-[:NEEDS]->(i)
+
+            WITH r, recipe
+            FOREACH (category IN recipe.categories |
+                MERGE (c:Category {name: category})
+                MERGE (r)-[:IS]->(c)
+            )
+            `, { recipes }
+        )
+    
+        await session.close()
+    }
 
     // const res = await axios('https://wikiwiki.jp/escha-logy/%E7%B4%A0%E6%9D%90')
     
