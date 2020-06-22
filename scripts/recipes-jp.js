a1eacf6f5d681eefc6cbf3f7f755f6ac8798f094
jurymeqcrime <jimmy.crequer@gmail.com>
2020-06-23 08:13:42 +0900

    [doing] Refactor recipes scraper

--- scripts/recipes-jp.js
+++ scripts/recipes-jp.js
@@ -7,15 +7,32 @@
     require('dotenv').config()
     const driver = neo4j.driver(`bolt://${process.env.NEO4J_HOST}`, neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PWD))
 
+    const itemIdx = {}
 
-    // resources
-
-    let res = await axios("https://omoteura.com/atelier_eschalogy/item-material.html")
+    let res = await axios("https://omoteura.com/atelier_eschalogy/item-list-category.html")
     let $ = cheerio.load(res.data)
 
+    data = $("h2").toArray()
+    for (let i = 0; i < data.length; i++) {
+      h2 = $(data[i])
 
-    let data = $("h2").toArray()
-    console.log(data.length)
+      let name = h2.text().trim()
+      name = name.replace("カテゴリが", "")
+      name = name.replace("のアイテム", "")
+
+      itemIdx[name] = { group: "CATEGORY", name }
+    }
+
+    // ================================================
+    // RESOURCES
+    // ================================================
+
+    res = await axios("https://omoteura.com/atelier_eschalogy/item-material.html")
+    $ = cheerio.load(res.data)
+
+
+    data = $("h2").toArray()
+  
 
     for (let i = 0; i < data.length; i++) {
         h2 = $(data[i])
@@ -24,68 +41,160 @@
 
         let dataArea = $(h2.next().next())
 
+        let level = $("tr:nth-child(3) > td:nth-child(1)", dataArea).text().trim()
         let categories = $("tr:nth-child(3) > td:nth-child(2)", dataArea).text().trim().split("、")
+        let attributes = [
+          $("tr:nth-child(3) > td:nth-child(3)", dataArea).text().trim(),
+          $("tr:nth-child(3) > td:nth-child(4)", dataArea).text().trim(),
+          $("tr:nth-child(3) > td:nth-child(5)", dataArea).text().trim(),
+          $("tr:nth-child(3) > td:nth-child(6)", dataArea).text().trim()
+        ]
 
-        const session = driver.session()
-        result = await session.run(
-          `
-            MERGE (i:Item {name: $name})
-            FOREACH (category IN $categories |
-              MERGE (c:Item:Category {name: category})
-              MERGE (c)-[:CONTAINS]->(i))
-          `, { name, categories }
-        )
-        await session.close()
+        // const session = driver.session()
+        // result = await session.run(
+        //   `
+        //     MERGE (i:Item {name: $name})
+        //     FOREACH (category IN $categories |
+        //       MERGE (c:Item:Category {name: category})
+        //       MERGE (c)-[:CONTAINS]->(i))
+        //   `, { name, categories }
+        // )
+        // await session.close()
 
-        console.log(name, categories)
+        itemIdx[name] = {
+          group: "RESOURCE",
+          name,
+          level,
+          categories,
+          attributes,
+          ingredients: []
+        }
+
+        // console.log(name, level, categories, attributes)
     }
 
     // await driver.close()
-    // returna
+    // return
 
-    const SOURCES = [
-        { url: "https://omoteura.com/atelier_eschalogy/item-use.html", heading: "h3"},
-        { url: "https://omoteura.com/atelier_eschalogy/item-mix.html", heading: "h2"}
-    ]
+    // ================================================
+    // MIX ITEMS
+    // ================================================
 
+    res = await axios("https://omoteura.com/atelier_eschalogy/item-mix.html")
+    $ = cheerio.load(res.data)
 
-    for (let u = 0; u < SOURCES.length; u++) {
-      res = await axios(SOURCES[u].url)
-      $ = cheerio.load(res.data)
+    data = $("h2").toArray()
+    for (let i = 0; i < data.length; i++) {
+      h2 = $(data[i])
 
+      const name = h2.text()
 
-      data = $(SOURCES[u].heading).toArray()
-      console.log(data.length)
+      let dataArea = $(h2.next().next())
 
-      for (let i = 0; i < data.length; i++) {
-          h3 = $(data[i])
+      let level = $("tr:nth-child(3) > td:nth-child(1)", dataArea).text().trim()
+      let attributes = [
+        $("tr:nth-child(3) > td:nth-child(4)", dataArea).text().trim(),
+        $("tr:nth-child(3) > td:nth-child(5)", dataArea).text().trim(),
+        $("tr:nth-child(3) > td:nth-child(6)", dataArea).text().trim(),
+        $("tr:nth-child(3) > td:nth-child(7)", dataArea).text().trim()
+      ]
+      let categories = $("tr:nth-child(3) > td:last-child", dataArea).text().trim().split("、")
+      
+      let ingredients = $("tr:nth-child(7) a", dataArea).toArray().filter(a => {
+        return $(a).attr("href").indexOf("item-book.html") == -1
+      }).map(a => $(a).text())
 
-          const name = h3.text()
-
-          let dataArea = $(h3.next().next())
-
-          let categories = $("tr:nth-child(3) > td:last-child", dataArea).text().trim().split("、")
-
-          let ingredients = $("tr:nth-child(7) a", dataArea).toArray().filter(a => {
-            return $(a).attr("href").indexOf("item-book.html") == -1
-          }).map(a => $(a).text())
-
-          const session = driver.session()
-          result = await session.run(
-            `
-              MERGE (i:Item {name: $name})
-              FOREACH (category IN $categories |
-                MERGE (c:Item:Category {name: category})
-                MERGE (c)-[:CONTAINS]->(i))
-              FOREACH (ingredient IN $ingredients |
-                MERGE (i2:Item {name: ingredient})
-                MERGE (i)-[:NEEDS]->(i2))
-            `, { name, categories, ingredients }
-          )
-          await session.close()
-
-          console.log(name, categories, ingredients)
+      itemIdx[name] = {
+        group: "MIX",
+        name,
+        level,
+        attributes,
+        categories,
+        ingredients
       }
+
+      // return
+    }    
+
+    // ================================================
+    // ATTACK & HEAL ITEMS
+    // ================================================
+
+    res = await axios("https://omoteura.com/atelier_eschalogy/item-use.html")
+    $ = cheerio.load(res.data)
+
+    data = $("h3").toArray()
+    for (let i = 0; i < data.length; i++) {
+      h3 = $(data[i])
+
+      const id = h3.attr("id")
+      const name = h3.text()
+
+      let dataArea = $(h3.next().next())
+
+      let level = $("tr:nth-child(3) > td:nth-child(1)", dataArea).text().trim()
+      let attributes = [
+        $("tr:nth-child(3) > td:nth-child(4)", dataArea).text().trim(),
+        $("tr:nth-child(3) > td:nth-child(5)", dataArea).text().trim(),
+        $("tr:nth-child(3) > td:nth-child(6)", dataArea).text().trim(),
+        $("tr:nth-child(3) > td:nth-child(7)", dataArea).text().trim()
+      ]
+      let categories = $("tr:nth-child(3) > td:last-child", dataArea).text().trim().split("、")
+
+      let ingredients = $("tr:nth-child(7) a", dataArea).toArray().filter(a => {
+        return $(a).attr("href").indexOf("item-book.html") == -1
+      }).map(a => $(a).text())
+
+      itemIdx[name] = {
+        group: id.indexOf("use01") > -1 ? "ATTACK" : "HEAL",
+        name,
+        level,
+        attributes,
+        categories,
+        ingredients
+      }
+    }
+
+    // ================================================
+    // VALIDATION
+    // ================================================
+
+    Object.values(itemIdx).forEach(obj => {
+      if (obj.ingredients) {
+        obj.ingredients.forEach(ingredient => {
+          if (!itemIdx[ingredient]) {
+            console.error(`[ERROR] Couldn't find required ingredient ${ingredient} for ${obj.name}`)
+            process.exit()
+          }
+        })
+      }
+    })
+
+    // ================================================
+    // SAVE TO DB
+    // ================================================
+
+    for (let i = 0; i < Object.values(itemIdx).length; i++) {
+      let obj = Object.values(itemIdx)[i]
+
+      if (obj.group == "CATEGORY")
+        continue
+      
+      const session = driver.session()
+        result = await session.run(
+          `
+            MERGE (n:Item {name: $name})
+            SET n.level = $level, n.attributes = $attributes, n.group = $group
+            FOREACH (category IN $categories |
+              MERGE (c:Category {name: category})
+              MERGE (c)-[:CONTAINS]->(n))
+            FOREACH (ingredient IN $ingredients |
+              MERGE (i {name: ingredient})
+              MERGE (n)-[:NEEDS]->(i))
+          `, obj
+        )
+        await session.close()
+    
     }
 
     await driver.close()
